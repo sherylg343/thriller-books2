e5ca1860f0479f830d3ed4a63a5f82539fba7a60
sherylg343 <sheryl.s.goldberg@gmail.com>
2020-10-06 02:22:46 +0000

    review form done except rating and added border below search row

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $(".dropdown-trigger").dropdown();
     $('.sidenav').sidenav();
     $('body').scrollspy({ target: '#nav-parent' })
+    $('#rateMe2').mdbRate();
   });
 
   $('select').formSelect();
